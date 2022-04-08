cb171d0bd7306e28ded9dd29b1fbf6f8e97e8a74
Felipe AKA Chewie <99052148+fdgalvan@users.noreply.github.com>
2022-04-07 20:15:32 -0500

    update info

--- seeders/20220406190526-items.js
+++ seeders/20220406190526-items.js
@@ -13,16 +13,26 @@ module.exports = {
     */
 
 
-    //  await queryInterface.bulkInsert('items', [{
-    //    category: 'furniture',
-    //    listingName: 'chair',
-    //    listingDescription: 'big chair',
-    //    listingPrice: 5,
-    //    inCart: false,
-    //    userID: 1,
-    //    createdAt: new Date(),
-    //    updatedAt: new Date()
-    //  }], {});
+     await queryInterface.bulkInsert('items', [{
+       category: 'furniture',
+       listingName: 'chair',
+       listingDescription: 'big chair',
+       listingPrice: 5,
+       inCart: false,
+       userID: 1,
+       createdAt: new Date(),
+       updatedAt: new Date()
+     },
+     {
+      category: 'furniture',
+      listingName: 'chair 2',
+      listingDescription: 'big chair',
+      listingPrice: 5,
+      inCart: false,
+      userID: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
   },
 
   async down (queryInterface, Sequelize) {
